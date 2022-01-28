68ae8208ea6199032ba2cd076b6ecce1d8f8c0d5
Alexander Bespoyasov <forjob159@gmail.com>
2022-01-28 17:56:43 +0100

    Use ESM in Webpack configuration

---
+++ game/webpack.config.js
@@ -0,0 +1,15 @@
+import webpack from 'webpack'
+import path from 'path'
+
+export default [{
+  name: 'client',
+  mode: 'production',
+  context: path.resolve(),
+  entry: {
+    javascript: ['./src/index.js']
+  },
+  output: {
+    filename: './js/bundle.js',
+    path: path.resolve('dist'),
+  },
+}]

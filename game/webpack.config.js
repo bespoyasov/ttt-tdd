8a39ad49d3adc507617a2a920b06b76e279e6a1c
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-15 12:14:46 +0200

    Apply Prettier to Webpack config

--- game/webpack.config.js
+++ game/webpack.config.js
@@ -1,15 +1,17 @@
-import webpack from 'webpack'
-import path from 'path'
+import webpack from "webpack";
+import path from "path";
 
-export default [{
-  name: 'client',
-  mode: 'production',
-  context: path.resolve(),
-  entry: {
-    javascript: ['./src/index.js']
+export default [
+  {
+    name: "client",
+    mode: "production",
+    context: path.resolve(),
+    entry: {
+      javascript: ["./src/index.js"],
+    },
+    output: {
+      filename: "./js/bundle.js",
+      path: path.resolve("dist"),
+    },
   },
-  output: {
-    filename: './js/bundle.js',
-    path: path.resolve('dist'),
-  },
-}]
+];
